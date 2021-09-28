6a41d2ebd3bdcdc2134deb418fd3957e29f1af67
gustavomarafon <gustavomarafom@gmail.com>
2021-09-27 21:21:33 -0300

    cigarrosExpress

---
+++ index.js
@@ -0,0 +1,101 @@
+const express = require('express')
+const cors = require('cors')
+const jwt = require('jsonwebtoken');
+const { pool } = require('./config')
+
+
+const app = express()
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors())
+
+const getCigarros = (request, response) => {
+    pool.query('SELECT * FROM cigarros', (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
+const addCigarro = (request, response) => {
+    const { marca, sabor, preco } = request.body
+
+    pool.query(
+        'INSERT INTO cigarros (marca, sabor, preco) VALUES ($1, $2, $3)',
+        [marca, sabor, preco],
+        (error) => {
+            if (error) {
+                console.log(error)
+                throw error
+            }
+            response.status(201).json({ status: 'success', message: 'Cigarro adicionado.' })
+        },
+    )
+}
+
+const updateCigarro = (request, response) => {
+    const { codigo, marca, sabor, preco } = request.body
+    pool.query('UPDATE cigarros set marca=$1, sabor=$2, preco=$3 where codigo=$4',
+        [marca, sabor, preco, codigo], error => {
+            if (error) {
+                console.log(error)
+                throw error
+            }
+            response.status(201).json({ status: 'success', message: 'Cigarro atualizado.' })
+        })
+}
+
+const deleteCigarro = (request, response, next) => {
+    const codigo = parseInt(request.params.id)
+    pool.query(
+        'DELETE from cigarros where codigo=$1',
+        [codigo],
+        (error, results) => {
+            if (error || results.rowCount == 0) {
+                return response.status(401).json({
+                    status: 'error',
+                    message: 'Não foi possivel remover o cigarro'
+                });
+            }
+            response.status(201).json({
+                status: 'success',
+                message: 'Cigarro removido com sucesso'
+            })
+        },
+    )
+}
+
+const getCigarroPorID = (request, response) => {
+    const codigo = parseInt(request.params.id)
+    pool.query('SELECT * FROM cigarros where codigo = $1',
+        [codigo], (error, results) => {
+            if (error || results.rowCount == 0) {
+                return response.status(401).json({
+                    status: 'error',
+                    message: 'Não foi possivel recuperar o cigarro'
+                });
+            }
+            response.status(200).json(results.rows)
+        })
+}
+
+    app
+        .route('/cigarros')
+        // GET endpoint
+        .get(getCigarros)
+        // POST endpoint
+        .post(addCigarro)
+        // PUT
+        .put(updateCigarro)
+
+    app.route('/cigarros/:id')
+        .get(getCigarroPorID) // chamada na url assim http://localhost:3002/produtos/2
+
+        .delete(deleteCigarro) // para delete é só chamar pelo método delete http://localhost:3002/produtos/2 2 é um código no BD
+
+    // Start server
+    app.listen(process.env.PORT || 3001, () => {
+        console.log(`Servidor rodando`)
+    })
